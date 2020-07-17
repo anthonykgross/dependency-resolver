312f0c3631fb79c55db65aa8ced9bf96a76dd544
Joshua Behrens <behrens@heptacom.de>
2020-07-17 02:25:14 +0200

    Add strict comparison

--- src/DependencyResolver.php
+++ src/DependencyResolver.php
@@ -34,8 +34,8 @@ class DependencyResolver
         $unresolved[] = $item;
 
         foreach ($items[$item] as $dep) {
-            if (!in_array($dep, $resolved)) {
-                if (!in_array($dep, $unresolved)) {
+            if (!in_array($dep, $resolved, true)) {
+                if (!in_array($dep, $unresolved, true)) {
                     $unresolved[] = $dep;
                     [$resolved, $unresolved] = self::resolver($dep, $items, $resolved, $unresolved);
                 } else {
@@ -44,11 +44,11 @@ class DependencyResolver
             }
         }
         // Add $item to $resolved if it's not already there
-        if (!in_array($item, $resolved)) {
+        if (!in_array($item, $resolved, true)) {
             $resolved[] = $item;
         }
         // Remove all occurrences of $item in $unresolved
-        while (($index = array_search($item, $unresolved)) !== false) {
+        while (($index = array_search($item, $unresolved, true)) !== false) {
             unset($unresolved[$index]);
         }
 
